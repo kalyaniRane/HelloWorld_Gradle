02bf15fcd9951caff02421c3eb8183f82d914616
kalyaniRane <kalyanirane19@gmail.com>
2020-07-07 20:19:48 +0530

    ADDED : Hello World Message

---
+++ src/main/java/com/example/demo/controller/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/hello")
+    public ResponseEntity hello(){
+        return new ResponseEntity("Hello World", HttpStatus.OK);
+    }
+
+}
