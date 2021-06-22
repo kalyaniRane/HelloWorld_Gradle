2fe2d02ab3ab93b608aee69b7c79ff5e1083b018
kalyaniRane <kalyanirane19@gmail.com>
2021-06-22 12:38:46 +0530

    MODIFIED : User API

--- src/main/java/com/example/demo/controller/HelloWorld.java
+++ src/main/java/com/example/demo/controller/HelloWorld.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,8 +15,8 @@ public class HelloWorld {
         return new ResponseEntity("Hello World", HttpStatus.OK);
     }
     
-    @GetMapping("/user")
-    public ResponseEntity user(){
+    @GetMapping("/user/{name}")
+    public ResponseEntity user(@PathVariable String name){
         return new ResponseEntity("John Demon", HttpStatus.OK);
     }	
 
