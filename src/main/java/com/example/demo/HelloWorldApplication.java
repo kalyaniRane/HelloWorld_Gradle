f0f08882faba44bddcd80e608688fda961d6a1fb
kalyaniRane <44054799+kalyaniRane@users.noreply.github.com>
2021-06-23 16:01:29 +0530

    REFACTORED :  Main Class

--- src/main/java/com/example/demo/HelloWorldApplication.java
+++ src/main/java/com/example/demo/HelloWorldApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldApplication {
 
     public static void main(String[] args) {
-	System.out.println("Hello Wolrd");
         SpringApplication.run(HelloWorldApplication.class, args);
     }
 
